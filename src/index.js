19fd460e62b55a2b43d188e41a4a134692e36268
alfaritsi21 <arqi.alfaritsi21@gmail.com>
2020-09-30 16:53:59 +0700

    feat: add configuration for express project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const route = require("express").Router();
+
+// const product = require("./routes/product");
+// const category = require("./routes/category");
+// const history = require("./routes/history");
+// const order = require("./routes/order");
+// const users = require("./routes/users");
+
+// route.use("/product", product);
+// route.use("/category", category);
+// route.use("/history", history);
+// route.use("/order", order);
+// route.use("/users", users);
+
+module.exports = route;
