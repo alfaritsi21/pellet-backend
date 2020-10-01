4ff8db08ff4b83bba507592b1f08873500c4a658
iqbalstwangit config ==global user.email iqbalstwan@yahoo.comgit config --global user.name iqbalstwan <iqbalstwan@yahoo.com>
2020-10-01 13:44:33 +0700

    added feat login and register

---
+++ src/controller/user.js
@@ -0,0 +1,242 @@
+const bcrypt = require("bcrypt");
+const helper = require("../helper");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const qs = require("querystring");
+const {
+  postUser,
+  checkUser,
+  checkNumber,
+  checkKey,
+  changePassword,
+} = require("../model/user");
+
+module.exports = {
+  register: async (request, response) => {
+    const { user_email, user_password, user_name, user_phone } = request.body;
+    const salt = bcrypt.genSaltSync(8);
+    const encryptPassword = bcrypt.hashSync(user_password, salt);
+    const checkEmail = await checkUser(request.body.user_email);
+    const checkPhone = await checkNumber(request.body.user_phone);
+    const setData = {
+      user_email,
+      user_password: encryptPassword,
+      user_name,
+      user_phone,
+      first_name: "",
+      last_name: "",
+      user_saldo: 0,
+      user_pin: 0,
+      user_img: "blank-profile.jpg",
+      user_created_at: new Date(),
+      user_status: 0,
+      user_key: 0,
+    };
+    try {
+      if (checkEmail.length > 0) {
+        return helper.response(response, 400, "Email is already registered");
+      } else if (checkPhone.length > 0) {
+        return helper.response(
+          response,
+          400,
+          "Phone Number is already registered"
+        );
+      } else if (!user_email.match("@")) {
+        return helper.response(response, 400, "Invalid,Missing Character('@')");
+      } else if (
+        setData.user_phone.length < 10 ||
+        setData.user_phone.length > 12
+      ) {
+        return helper.response(response, 400, "Invalid phone number");
+      } else if (
+        request.body.user_password.length < 8 ||
+        request.body.user_password.length > 16
+      ) {
+        return helper.response(
+          response,
+          400,
+          "Password must be 8-16 characters"
+        );
+      } else if (request.body.confirm_password !== request.body.user_password) {
+        return helper.response(response, 400, "Password didn't match");
+      } else {
+        const result = await postUser(setData);
+        console.log(result);
+        return helper.response(response, 200, "Success Register User", result);
+      }
+    } catch (error) {
+      console.log(error);
+      return helper.response(response, 400, "Bad Request");
+    }
+  },
+  loginUser: async (request, response) => {
+    if (
+      request.body.user_email === undefined ||
+      request.body.user_email === ""
+    ) {
+      return helper.response(response, 400, "Email must be filled");
+    } else if (
+      request.body.user_password === undefined ||
+      request.body.user_password === ""
+    ) {
+      return helper.response(response, 400, "Password must be filled");
+    }
+    try {
+      const checkDataUser = await checkUser(request.body.user_email);
+      if (checkDataUser.length >= 1) {
+        const checkPassword = bcrypt.compareSync(
+          request.body.user_password,
+          checkDataUser[0].user_password
+        );
+        if (checkPassword) {
+          let payload = {
+            user_id: checkDataUser[0].user_id,
+            user_email: checkDataUser[0].user_email,
+            user_name: checkDataUser[0].user_name,
+            user_img: checkDataUser[0].user_img,
+            user_pin: checkDataUser[0].user_pin,
+            user_phone: checkDataUser[0].user_phone,
+            user_status: checkDataUser[0].user_status,
+            user_key: checkDataUser[0].user_key,
+          };
+          //   if (payload.user_status === 0) {
+          //     return helper.response(
+          //       response,
+          //       400,
+          //       "Your account is not activated"
+          //     );
+          //   } else {
+          const token = jwt.sign(payload, "RAHASIA", { expiresIn: "24h" });
+          payload = { ...payload, token };
+          return helper.response(response, 200, "Success login", payload);
+          //   }
+        } else {
+          return helper.response(response, 400, "Wrong password !");
+        }
+      } else {
+        return helper.response(response, 400, "Email is not registered !");
+      }
+    } catch (error) {
+      return helper.response(response, 400, "Bad Request");
+    }
+  },
+  forgotPassword: async (request, response) => {
+    try {
+      const { user_email } = request.body;
+      const keys = Math.round(Math.random() * 100000);
+      const checkDataUser = await checkUser(user_email);
+      if (checkDataUser.length >= 1) {
+        const data = {
+          user_key: keys,
+          user_updated_at: new Date(),
+        };
+        await changePassword(data, user_email);
+        const transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.USER,
+            pass: process.env.PASS,
+          },
+        });
+        await transporter.sendMail({
+          from: '"Pellet"',
+          to: user_email,
+          subject: "Pellet - Forgot Password",
+          html: `your code is <b>${keys}</b>`,
+          //   `<a href="http://localhost:8080/setpassword?keys=${keys}">Click Here To Change Password</a>`,
+        }),
+          function (error) {
+            if (error) {
+              return helper.response(response, 400, "Email not sent !");
+            }
+          };
+        return helper.response(response, 200, "Email has been sent !");
+      } else {
+        return helper.response(response, 400, "Email is not registered !");
+      }
+    } catch (error) {
+      console.log(error);
+      return helper.response(response, 400, "Bad Request", error);
+    }
+  },
+  changePass: async (request, response) => {
+    try {
+      const { keys } = request.query;
+      const { user_password } = request.body;
+      const checkDataUser = await checkKey(keys);
+      if (
+        request.query.keys === undefined ||
+        request.query.keys === null ||
+        request.query.keys === ""
+      ) {
+        return helper.response(response, 400, "Invalid Key");
+      }
+      if (checkDataUser.length > 0) {
+        const email = checkDataUser[0].user_email;
+        let setData = {
+          user_key: keys,
+          user_password,
+          user_updated_at: new Date(),
+        };
+        const difference =
+          setData.user_updated_at - checkDataUser[0].user_updated_at;
+        const minutesDifference = Math.floor(difference / 1000 / 60);
+        if (minutesDifference > 5) {
+          const data = {
+            user_key: "",
+            user_updated_at: new Date(),
+          };
+          await changePassword(data, email);
+          return helper.response(response, 400, "Key has expired");
+        } else if (
+          request.body.user_password === undefined ||
+          request.body.user_password === null ||
+          request.body.user_password === ""
+        ) {
+          return helper.response(response, 400, "Password must be filled !");
+        } else if (
+          request.body.confirm_password === undefined ||
+          request.body.confirm_password === null ||
+          request.body.confirm_password === ""
+        ) {
+          return helper.response(
+            response,
+            400,
+            "Confirm Password must be filled !"
+          );
+        } else if (
+          request.body.user_password.length < 8 ||
+          request.body.user_password.length > 16
+        ) {
+          return helper.response(
+            response,
+            400,
+            "Password must be 8-16 characters"
+          );
+        } else if (
+          request.body.confirm_password !== request.body.user_password
+        ) {
+          return helper.response(response, 400, "Password didn't match");
+        } else {
+          const salt = bcrypt.genSaltSync(10);
+          const encryptPassword = bcrypt.hashSync(user_password, salt);
+          setData.user_password = encryptPassword;
+          setData.user_key = "";
+        }
+        const result = await changePassword(setData, email);
+        return helper.response(
+          response,
+          200,
+          "Success Password Updated",
+          result
+        );
+      } else {
+        return helper.response(response, 404, `Invalid key`);
+      }
+    } catch (error) {
+      return helper.response(response, 404, "Bad Request", error);
+    }
+  },
+};
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const route = require("express").Router();
 // const category = require("./routes/category");
 // const history = require("./routes/history");
 // const order = require("./routes/order");
-// const users = require("./routes/users");
+const user = require("./routes/user");
+// const payment = require("./routes/payment");
 
 // route.use("/product", product);
 // route.use("/category", category);
 // route.use("/history", history);
 // route.use("/order", order);
-// route.use("/users", users);
+route.use("/user", user);
+// route.use("/payment, payment);
 
 module.exports = route;
