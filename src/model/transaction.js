4578ee5817d405d182e60e707187beadc5b7e4c0
Glitchfer <arif.0496.rahman@gmail.com>
2020-10-05 00:53:06 +0700

    Update Transaction History

--- src/model/transaction.js
+++ src/model/transaction.js
@@ -54,7 +54,7 @@ module.exports = {
   getTransact: (id, date_from, date_to) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT * FROM transaction LEFT JOIN user ON IF(transaction.user_id = ${id}, transaction.target_id = user.user_id, transaction.user_id = user.user_id ) WHERE transaction.user_id = ${id} || transaction.target_id = ${id} AND created_at >= "${date_from}" AND created_at <= "${date_to}" ORDER BY created_at ASC`,
+        `SELECT trans_id, transaction.user_id, transaction.target_id, transaction.trans_type, transaction.trans_nominal, DATE_FORMAT(transaction.created_at, '%d-%m-%Y') as created_at, DATE_FORMAT(transaction.created_at, '%H:%i:%S') as time, user.user_id, user.user_email, user.user_name, user.user_phone, user.user_img, user.first_name, user.last_name FROM transaction JOIN user ON IF(transaction.user_id = ${id}, transaction.target_id = user.user_id, transaction.user_id = user.user_id ) WHERE transaction.user_id = ${id} || transaction.target_id = ${id} AND (created_at >= "${date_from}" AND created_at <= "${date_to}") ORDER BY trans_id ASC`,
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error));
         }
