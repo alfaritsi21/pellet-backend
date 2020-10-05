a6b38e3d72641c48c501f698b3f3c8b7ceef43df
Glitchfer <arif.0496.rahman@gmail.com>
2020-10-05 16:19:00 +0700

    update transaction & upload img

--- src/model/transaction.js
+++ src/model/transaction.js
@@ -24,8 +24,9 @@ module.exports = {
   getIncomeTransactionPerDay: (date, user) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT SUM(trans_nominal) as income, DATE(created_at) as date FROM transaction WHERE DATE(created_at) > "${date}" AND target_id = ${user} GROUP BY DATE(created_at)`,
+        `SELECT SUM(trans_nominal) as income, DATE_FORMAT(created_at, '%Y-%m-%d') as date FROM transaction WHERE DATE(created_at) > "${date}" AND target_id = ${user} GROUP BY DATE(created_at)`,
         (error, result) => {
+          console.log(result);
           !error ? resolve(result) : reject(new Error(error));
         }
       );
@@ -44,7 +45,7 @@ module.exports = {
   getExpenseTransactionPerDay: (date, user) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT SUM(trans_nominal) as expense, DATE(created_at) as date FROM transaction WHERE DATE(created_at) > "${date}" AND user_id = ${user} GROUP BY DATE(created_at)`,
+        `SELECT SUM(trans_nominal) as expense, DATE_FORMAT(created_at, '%Y-%m-%d') as date FROM transaction WHERE DATE(created_at) > "${date}" AND user_id = ${user} GROUP BY DATE(created_at)`,
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error));
         }
@@ -54,7 +55,7 @@ module.exports = {
   getTransact: (id, date_from, date_to) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT trans_id, transaction.user_id, transaction.target_id, transaction.trans_type, transaction.trans_nominal, transaction.created_at, user.user_id, user.user_email, user.user_name, user.user_phone, user.user_img, user.first_name, user.last_name FROM transaction JOIN user ON IF(transaction.user_id = ${id}, transaction.target_id = user.user_id, transaction.user_id = user.user_id ) WHERE (transaction.user_id = ${id} || transaction.target_id = ${id}) AND (created_at >= "${date_from}" AND created_at <= "${date_to}") ORDER BY trans_id ASC`,
+        `SELECT trans_id, transaction.user_id, transaction.target_id, transaction.trans_type, transaction.trans_nominal, transaction.created_at, user.user_id, user.user_email, user.user_name, user.user_phone, user.user_img, user.first_name, user.last_name FROM transaction JOIN user ON IF(transaction.user_id = ${id}, transaction.target_id = user.user_id, transaction.user_id = user.user_id ) WHERE (transaction.user_id = ${id} || transaction.target_id = ${id}) AND (created_at >= "${date_from}" AND created_at <= "${date_to}") ORDER BY trans_id DESC`,
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error));
         }
