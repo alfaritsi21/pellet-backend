648a084a01a31d2cb998aa2b60dcdba0ada46bb7
alfaritsi21 <arqi.alfaritsi21@gmail.com>
2020-10-05 19:59:56 +0700

    feat: add endpoint for midtrans payment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const { postPayment, postMidtransNotif } = require("../controller/payment");
 
-router.post("/");
-router.post("/midtrans-notif");
+router.post("/", postPayment);
+router.post("/midtrans-notif", postMidtransNotif);
 
 module.exports = router;
